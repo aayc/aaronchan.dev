01a184ef7146c6341d73e085c1fd3acb4498e55c
Aaron Chan <aaronchan@microsoft.com>
2023-03-12 16:37:39 -0600

    Fix formatting

--- pages/admin/home.tsx
+++ pages/admin/home.tsx
@@ -1,42 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import LoginForm from "../../components/LoginForm"
+import LoginForm from "../../components/LoginForm";
 import { auth } from "../../utils/firebase";
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 import moment from "moment";
 
 function Admin() {
-    return (
-        <div className="mt-12">
-            <div className="text-center text-2xl">
-                {moment().format("MMMM Do, YYYY")}
-            </div>
-            <div>
-                Did you do 20 push ups?
-            </div>
-        </div>
-    )
+  return (
+    <div className="mt-12">
+      <div className="text-center text-2xl">
+        {moment().format("MMMM Do, YYYY")}
+      </div>
+      <div>Did you do 20 push ups?</div>
+    </div>
+  );
 }
 
 function AdminHome() {
-    const [user, loading, error] = useAuthState(auth);
-    const widget = () => {
-        if (loading) {
-            return <div>Loading...</div>
-        } else if (user) {
-            return Admin()
-        } else if (error) {
-            return <div>Error: {error.message}</div>
-        } else {
-            return <LoginForm></LoginForm>
-        }
+  const [user, loading, error] = useAuthState(auth);
+  const widget = () => {
+    if (loading) {
+      return <div>Loading...</div>;
+    } else if (user) {
+      return Admin();
+    } else if (error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return <LoginForm></LoginForm>;
     }
+  };
 
   return (
     <div>
-      <div className="m-auto max-w-7xl">
-        {widget()}
-      </div>
+      <div className="m-auto max-w-7xl">{widget()}</div>
     </div>
   );
 }
